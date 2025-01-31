55837d722a1442e7aaeca4283ce621ac3fb22cb2
divital-coder <divital2004@gmail.com>
2025-01-31 12:21:12 +0530

    Added support for crossection calculations

--- gen/cpp/HepMC3WrapImpl.cpp
+++ gen/cpp/HepMC3WrapImpl.cpp
@@ -1,4 +1,12 @@
 #include "HepMC3Wrap.h"
+#include <memory>
+#include <unordered_map>
+#include <string>
+
+// Global storage for shared pointers to maintain ownership
+static std::unordered_map<void*, std::shared_ptr<HepMC3::GenParticle>> particle_store;
+static std::unordered_map<void*, std::shared_ptr<HepMC3::GenVertex>> vertex_store;
+static std::unordered_map<void*, std::shared_ptr<HepMC3::GenCrossSection>> xs_store;
 
 // Implementation of the C-style interface
 extern "C" {
@@ -8,6 +16,102 @@ extern "C" {
 
     void* create_genevent(int units) {
         return new HepMC3::GenEvent(static_cast<HepMC3::Units::MomentumUnit>(units));
+    }
+
+    void* create_particle(void* momentum, int pdg_id, int status) {
+        auto* mom = static_cast<HepMC3::FourVector*>(momentum);
+        // Create particle with shared ownership
+        auto particle = std::make_shared<HepMC3::GenParticle>(*mom, pdg_id, status);
+        void* ptr = particle.get();
+        // Store the shared_ptr
+        particle_store[ptr] = particle;
+        return ptr;
+    }
+
+    void* create_vertex() {
+        auto vertex = std::make_shared<HepMC3::GenVertex>();
+        void* ptr = vertex.get();
+        // Store the shared_ptr
+        vertex_store[ptr] = vertex;
+        return ptr;
+    }
+
+    void add_particle_in(void* vertex, void* particle) {
+        auto v_it = vertex_store.find(vertex);
+        auto p_it = particle_store.find(particle);
+        if (v_it != vertex_store.end() && p_it != particle_store.end()) {
+            v_it->second->add_particle_in(p_it->second);
+        }
+    }
+
+    void add_particle_out(void* vertex, void* particle) {
+        auto v_it = vertex_store.find(vertex);
+        auto p_it = particle_store.find(particle);
+        if (v_it != vertex_store.end() && p_it != particle_store.end()) {
+            v_it->second->add_particle_out(p_it->second);
+        }
+    }
+
+    void set_vertex_status(void* vertex, int status) {
+        auto v_it = vertex_store.find(vertex);
+        if (v_it != vertex_store.end()) {
+            v_it->second->set_status(status);
+        }
+    }
+
+    int vertex_status(void* vertex) {
+        auto v_it = vertex_store.find(vertex);
+        if (v_it != vertex_store.end()) {
+            return v_it->second->status();
+        }
+        return 0;
+    }
+
+    void add_vertex_to_event(void* event, void* vertex) {
+        auto* evt = static_cast<HepMC3::GenEvent*>(event);
+        auto v_it = vertex_store.find(vertex);
+        if (v_it != vertex_store.end()) {
+            evt->add_vertex(v_it->second);
+        }
+    }
+
+    int particle_pid(void* particle) {
+        auto p_it = particle_store.find(particle);
+        if (p_it != particle_store.end()) {
+            return p_it->second->pid();
+        }
+        return 0;
+    }
+
+    int particle_status(void* particle) {
+        auto p_it = particle_store.find(particle);
+        if (p_it != particle_store.end()) {
+            return p_it->second->status();
+        }
+        return 0;
+    }
+
+    void* particle_momentum(void* particle) {
+        auto p_it = particle_store.find(particle);
+        if (p_it != particle_store.end()) {
+            return new HepMC3::FourVector(p_it->second->momentum());
+        }
+        return nullptr;
+    }
+
+    void set_particle_status(void* particle, int status) {
+        auto p_it = particle_store.find(particle);
+        if (p_it != particle_store.end()) {
+            p_it->second->set_status(status);
+        }
+    }
+
+    void set_particle_momentum(void* particle, void* momentum) {
+        auto p_it = particle_store.find(particle);
+        if (p_it != particle_store.end()) {
+            auto* mom = static_cast<HepMC3::FourVector*>(momentum);
+            p_it->second->set_momentum(*mom);
+        }
     }
 
     double fourvector_px(void* v) {
@@ -25,4 +129,103 @@ extern "C" {
     double fourvector_e(void* v) {
         return static_cast<HepMC3::FourVector*>(v)->e();
     }
+
+
+
+    
+    void* create_cross_section() {
+        auto xs = std::make_shared<HepMC3::GenCrossSection>();
+        void* ptr = xs.get();
+        xs_store[ptr] = xs;
+        return ptr;
+    }
+
+    void set_cross_section(void* xs, double value, double error) {
+        auto xs_it = xs_store.find(xs);
+        if (xs_it != xs_store.end()) {
+            xs_it->second->set_cross_section(value, error);
+        }
+    }
+
+    double get_cross_section(void* xs) {
+        auto xs_it = xs_store.find(xs);
+        if (xs_it != xs_store.end()) {
+            return xs_it->second->xsec();
+        }
+        return 0.0;
+    }
+
+    double get_cross_section_error(void* xs) {
+        auto xs_it = xs_store.find(xs);
+        if (xs_it != xs_store.end()) {
+            return xs_it->second->xsec_err();
+        }
+        return 0.0;
+    }
+
+    void add_cross_section_to_event(void* event, void* xs) {
+        auto* evt = static_cast<HepMC3::GenEvent*>(event);
+        auto xs_it = xs_store.find(xs);
+        if (xs_it != xs_store.end()) {
+            evt->set_cross_section(xs_it->second);
+        }
+    }
+
+    void set_cross_section_alternative(void* xs, double value, double error, int id) {
+        auto xs_it = xs_store.find(xs);
+        if (xs_it != xs_store.end()) {
+            // Convert id to string for named cross section
+            std::string name = "alternative_" + std::to_string(id);
+            xs_it->second->set_xsec(name, value);
+            // Set the error separately if needed
+            // Note: HepMC3 doesn't directly support setting errors for alternative cross sections
+        }
+    }
+
+    double get_cross_section_alternative(void* xs, int id) {
+        auto xs_it = xs_store.find(xs);
+        if (xs_it != xs_store.end()) {
+            std::string name = "alternative_" + std::to_string(id);
+            return xs_it->second->xsec(name);
+        }
+        return 0.0;
+    }
+
+    double get_cross_section_error_alternative(void* xs, int id) {
+        // Note: HepMC3 doesn't directly support getting errors for alternative cross sections
+        // We'll return 0.0 as a default
+        return 0.0;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/HepMC3.jl
+++ src/HepMC3.jl
@@ -38,6 +38,10 @@ mutable struct GenEvent <: AbstractHepMC3Type
     ptr::Ptr{Cvoid}
 end
 
+mutable struct GenParticle <: AbstractHepMC3Type
+    ptr::Ptr{Cvoid}
+end
+
 # Constructor functions
 """
     FourVector(x::Float64, y::Float64, z::Float64, t::Float64)
@@ -58,7 +62,17 @@ function GenEvent(units::Int = -1)
     GenEvent(ptr)
 end
 
-# Add accessor functions
+"""
+    GenParticle(momentum::FourVector, pdg_id::Int, status::Int)
+Create a new particle with given momentum, PDG ID, and status code.
+"""
+function GenParticle(momentum::FourVector, pdg_id::Int, status::Int)
+    ptr = ccall((:create_particle, libpath), Ptr{Cvoid},
+                (Ptr{Cvoid}, Int32, Int32), momentum.ptr, pdg_id, status)
+    GenParticle(ptr)
+end
+
+# Add accessor functions for FourVector
 function px(v::FourVector)
     ccall((:fourvector_px, libpath), Float64, (Ptr{Cvoid},), v.ptr)
 end
@@ -75,8 +89,222 @@ function e(v::FourVector)
     ccall((:fourvector_e, libpath), Float64, (Ptr{Cvoid},), v.ptr)
 end
 
+# Add accessor functions for GenParticle
+"""
+    pid(particle::GenParticle)
+Get the PDG ID of a particle.
+"""
+function pid(particle::GenParticle)
+    ccall((:particle_pid, libpath), Int32, (Ptr{Cvoid},), particle.ptr)
+end
+
+"""
+    status(particle::GenParticle)
+Get the status code of a particle.
+"""
+function status(particle::GenParticle)
+    ccall((:particle_status, libpath), Int32, (Ptr{Cvoid},), particle.ptr)
+end
+
+"""
+    momentum(particle::GenParticle)
+Get the four-momentum of a particle.
+"""
+function momentum(particle::GenParticle)
+    ptr = ccall((:particle_momentum, libpath), Ptr{Cvoid}, (Ptr{Cvoid},), particle.ptr)
+    FourVector(ptr)
+end
+
+"""
+    set_status!(particle::GenParticle, status::Int)
+Set the status code of a particle.
+"""
+function set_status!(particle::GenParticle, status::Int)
+    ccall((:set_particle_status, libpath), Cvoid,
+          (Ptr{Cvoid}, Int32), particle.ptr, status)
+end
+
+"""
+    set_momentum!(particle::GenParticle, momentum::FourVector)
+Set the four-momentum of a particle.
+"""
+function set_momentum!(particle::GenParticle, momentum::FourVector)
+    ccall((:set_particle_momentum, libpath), Cvoid,
+          (Ptr{Cvoid}, Ptr{Cvoid}), particle.ptr, momentum.ptr)
+end
+
+
+
+# Add new type
+mutable struct GenVertex <: AbstractHepMC3Type
+    ptr::Ptr{Cvoid}
+end
+
+# Add new functions
+"""
+    GenVertex()
+Create a new vertex.
+"""
+function GenVertex()
+    ptr = ccall((:create_vertex, libpath), Ptr{Cvoid}, ())
+    GenVertex(ptr)
+end
+
+"""
+    add_particle_in!(vertex::GenVertex, particle::GenParticle)
+Add an incoming particle to the vertex.
+"""
+function add_particle_in!(vertex::GenVertex, particle::GenParticle)
+    ccall((:add_particle_in, libpath), Cvoid,
+          (Ptr{Cvoid}, Ptr{Cvoid}), vertex.ptr, particle.ptr)
+end
+
+"""
+    add_particle_out!(vertex::GenVertex, particle::GenParticle)
+Add an outgoing particle to the vertex.
+"""
+function add_particle_out!(vertex::GenVertex, particle::GenParticle)
+    ccall((:add_particle_out, libpath), Cvoid,
+          (Ptr{Cvoid}, Ptr{Cvoid}), vertex.ptr, particle.ptr)
+end
+
+"""
+    vertex_status(vertex::GenVertex)
+Get the status code of a vertex.
+"""
+function vertex_status(vertex::GenVertex)
+    ccall((:vertex_status, libpath), Int32, (Ptr{Cvoid},), vertex.ptr)
+end
+
+"""
+    set_vertex_status!(vertex::GenVertex, status::Int)
+Set the status code of a vertex.
+"""
+function set_vertex_status!(vertex::GenVertex, status::Int)
+    ccall((:set_vertex_status, libpath), Cvoid,
+          (Ptr{Cvoid}, Int32), vertex.ptr, status)
+end
+
+"""
+    add_vertex!(event::GenEvent, vertex::GenVertex)
+Add a vertex to an event.
+"""
+function add_vertex!(event::GenEvent, vertex::GenVertex)
+    ccall((:add_vertex_to_event, libpath), Cvoid,
+          (Ptr{Cvoid}, Ptr{Cvoid}), event.ptr, vertex.ptr)
+end
+
+
+
+
+
+
+
+
+
+# Add new type
+mutable struct GenCrossSection <: AbstractHepMC3Type
+    ptr::Ptr{Cvoid}
+end
+
+"""
+    GenCrossSection()
+
+Create a new cross-section object.
+"""
+function GenCrossSection()
+    ptr = ccall((:create_cross_section, libpath), Ptr{Cvoid}, ())
+    GenCrossSection(ptr)
+end
+
+"""
+    set_cross_section!(xs::GenCrossSection, value::Float64, error::Float64)
+
+Set the cross-section value and its error.
+"""
+function set_cross_section!(xs::GenCrossSection, value::Float64, error::Float64)
+    ccall((:set_cross_section, libpath), Cvoid,
+          (Ptr{Cvoid}, Float64, Float64), xs.ptr, value, error)
+end
+
+"""
+    cross_section(xs::GenCrossSection)
+
+Get the cross-section value.
+"""
+function cross_section(xs::GenCrossSection)
+    ccall((:get_cross_section, libpath), Float64, (Ptr{Cvoid},), xs.ptr)
+end
+
+"""
+    cross_section_error(xs::GenCrossSection)
+
+Get the cross-section error.
+"""
+function cross_section_error(xs::GenCrossSection)
+    ccall((:get_cross_section_error, libpath), Float64, (Ptr{Cvoid},), xs.ptr)
+end
+
+"""
+    add_cross_section!(event::GenEvent, xs::GenCrossSection)
+
+Add a cross-section to an event.
+"""
+function add_cross_section!(event::GenEvent, xs::GenCrossSection)
+    ccall((:add_cross_section_to_event, libpath), Cvoid,
+          (Ptr{Cvoid}, Ptr{Cvoid}), event.ptr, xs.ptr)
+end
+
+"""
+    set_alternative_cross_section!(xs::GenCrossSection, value::Float64, error::Float64, id::Int)
+
+Set an alternative cross-section value with a specific ID.
+Note: The error parameter is currently not used as HepMC3 doesn't support separate errors for alternative cross-sections.
+"""
+function set_alternative_cross_section!(xs::GenCrossSection, value::Float64, error::Float64, id::Int)
+    ccall((:set_cross_section_alternative, libpath), Cvoid,
+          (Ptr{Cvoid}, Float64, Float64, Int32), xs.ptr, value, error, id)
+end
+
+"""
+    alternative_cross_section(xs::GenCrossSection, id::Int)
+
+Get an alternative cross-section value by ID.
+"""
+function alternative_cross_section(xs::GenCrossSection, id::Int)
+    ccall((:get_cross_section_alternative, libpath), Float64,
+          (Ptr{Cvoid}, Int32), xs.ptr, id)
+end
+
+"""
+    alternative_cross_section_error(xs::GenCrossSection, id::Int)
+Get an alternative cross-section error by ID.
+Note: This always returns 0.0 as HepMC3 doesn't support separate errors for alternative cross-sections.
+"""
+function alternative_cross_section_error(xs::GenCrossSection, id::Int)
+    # ccall((:get_cross_section_error_alternative, libpath), Float64,
+    #       (Ptr{Cvoid}, Int32), xs.ptr, id)
+    @info "HepMC3 doesn't support separate errors for alternative cross-sections."
+    return 0.0
+end
+
+# Add exports
+export GenCrossSection
+export set_cross_section!, cross_section, cross_section_error
+export add_cross_section!
+export set_alternative_cross_section!, alternative_cross_section, alternative_cross_section_error
+
+
+# Add exports
+export GenVertex
+export add_particle_in!, add_particle_out!
+export vertex_status, set_vertex_status!
+export add_vertex!
+
+
 # Export the interface
 export FourVector, GenEvent, GenVertex, GenParticle
 export px, py, pz, e
+export pid, status, momentum, set_status!, set_momentum!
 
 end # module
